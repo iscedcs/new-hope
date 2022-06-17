fee0ac54945bd00fa79383452a1f95ff686bf2a1
Ayobami Oyeniran Paul <ap.oyeniran@gmail.com>
2022-06-17 08:57:31 +0100

    new

--- js/forminput.js
+++ js/forminput.js
@@ -1,93 +1,137 @@
-document.addEventListener('submit', function (event) {
+function getItemValue(item){
+     if(document.getElementById(item)) 
+          return document.getElementById(item).value || 0;
+     return 0;
+}
+let bookNow = document.querySelector("#bookNow");
+bookNow.addEventListener('click', function(event) {
      event.preventDefault();
-});
+})
 
 let newHopeForm = document.getElementById('bookingform');
 newHopeForm.addEventListener('input', bookingObject);
+
+
+
+
 function bookingObject(event){
+
+     function getTotalPrice(){
+          let totalPrice = parseInt(getItemValue("servicetype")) + parseInt(getItemValue("bathroom_no")) + parseInt(getItemValue("bedroom_no")) + parseInt(getItemValue("dirtiness")) + parseInt(getItemValue("kitchen_no")) + parseInt(extinsideoven) + parseInt(extwalls) + parseInt(extinsideWindow) + parseInt(extinsideTheFridge) + parseInt(extinsideCabinets) + parseInt(extorganization) + parseInt(extinsideDishwasher) + parseInt(extinsideGarage) + parseInt(extmicrowave) + parseInt(extlaundry) + parseInt(extblinds) + parseInt(extinsideWasherDryer) ;
+          if (document.querySelector('input[name = "how_often"]:checked').value == 1) {
+               totalPrice *= 1
+          } else if (document.querySelector('input[name = "how_often"]:checked').value == 2){
+               totalPrice *= 2
+          } else if (document.querySelector('input[name = "how_often"]:checked').value == 4)(
+               totalPrice *=4
+          );
+          addDiscountCode();
+          return totalPrice;
+     
+          function addDiscountCode() {
+               if (getItemValue('discount_code') === "DISCOUNT5") {
+                    totalPrice = (totalPrice - (totalPrice * 0.05));
+               }
+               else if (getItemValue('discount_code') == "DISCOUNT10") {
+                    totalPrice = (totalPrice - (totalPrice * 0.1));
+               }
+               else if (getItemValue('discount_code') == "DISCOUNT15") {
+                    totalPrice = (totalPrice - (totalPrice * 0.15));
+               }
+               else if (getItemValue('discount_code') == "DISCOUNT20") {
+                    totalPrice = (totalPrice - (totalPrice * 0.2));
+               }
+               else if (getItemValue('discount_code') == "DISCOUNT90") {
+                    totalPrice = (totalPrice - (totalPrice * 0.9));
+               } else {
+                    totalPrice = totalPrice;
+               }
+          }
+     }
+     
      let expdate = document.getElementById('expiry_date').value.split("/");
      let expiry_month = expdate[0];
      let expiry_year = expdate[1];
      let tot_price = "500,000";
      let extras_insideOven = document.querySelector('#inside_oven');
      if (extras_insideOven.checked) {
-          extinsideoven = document.getElementById('inside_oven').value
+          extinsideoven = document.querySelector('#inside_oven').value
      } else {
           extinsideoven = "0";
      };
      let extras_walls = document.querySelector('#walls');
      if (extras_walls.checked) {
-          extwalls = document.getElementById('walls').value
+          extwalls = document.querySelector('#walls').value
      } else {
           extwalls = "0";
      };
      let extras_insideWindow = document.querySelector('#inside_window');
      if (extras_insideWindow.checked) {
-          extinsideWindow = document.getElementById('inside_window').value
+          extinsideWindow = document.querySelector('#inside_window').value
      } else {
           extinsideWindow = "0";
      };
      let extras_insideTheFridge = document.querySelector('#inside_the_fridge');
      if (extras_insideTheFridge.checked) {
-          extinsideTheFridge = document.getElementById('inside_the_fridge').value
+          extinsideTheFridge = document.querySelector('#inside_the_fridge').value
      } else {
           extinsideTheFridge = "0";
      };
      let extras_insideCabinets = document.querySelector('#inside_cabinets');
      if (extras_insideCabinets.checked) {
-          extinsideCabinets = document.getElementById('inside_cabinets').value
+          extinsideCabinets = document.querySelector('#inside_cabinets').value
      } else {
           extinsideCabinets = "0";
      };
      let extras_organization = document.querySelector('#organization');
      if (extras_organization.checked) {
-          extorganization = document.getElementById('organization').value
+          extorganization = document.querySelector('#organization').value
      } else {
           extorganization = "0";
      };
      let extras_insideDishwasher = document.querySelector('#inside_dishwasher');
      if (extras_insideDishwasher.checked) {
-          extinsideDishwasher = document.getElementById('inside_dishwasher').value
+          extinsideDishwasher = document.querySelector('#inside_dishwasher').value
      } else {
           extinsideDishwasher = "0";
      };
      let extras_insideGarage = document.querySelector('#inside_garage');
      if (extras_insideGarage.checked) {
-          extinsideGarage = document.getElementById('inside_garage').value
+          extinsideGarage = document.querySelector('#inside_garage').value
      } else {
           extinsideGarage = "0";
      };
      let extras_microwave = document.querySelector('#microwave');
      if (extras_microwave.checked) {
-          extmicrowave = document.getElementById('microwave').value
+          extmicrowave = document.querySelector('#microwave').value
      } else {
           extmicrowave = "0";
      };
      let extras_laundry = document.querySelector('#laundry');
      if (extras_laundry.checked) {
-          extlaundry = document.getElementById('laundry').value
+          extlaundry = document.querySelector('#laundry').value
      } else {
           extlaundry = "0";
      };
      let extras_blinds = document.querySelector('#blinds');
      if (extras_blinds.checked) {
-          extblinds = document.getElementById('blinds').value
+          extblinds = document.querySelector('#blinds').value
      } else {
           extblinds = "0";
      };
      let extras_insideWasherDryer = document.querySelector('#inside_washer_dryer');
      if (extras_insideWasherDryer.checked) {
-          extinsideWasherDryer = document.getElementById('inside_washer_dryer').value
+          extinsideWasherDryer = document.querySelector('#inside_washer_dryer').value
      } else {
           extinsideWasherDryer = "0";
      };
      const pay = {
-               "service_type": document.getElementById('servicetype').value,
+               "service_type": getItemValue('servicetype'),
                "home": {
-                    "bathroom": document.getElementById('bathroom_no').value,
-                    "bedroom": document.getElementById('bedroom_no').value,
-                    "dirtiness": document.getElementById('dirtiness').value,
-                    "kitchen": document.getElementById('kitchen_no').value
+                    "bathroom": getItemValue('bathroom_no'),
+                    "bedroom": getItemValue('bedroom_no'),
+                    "dirtiness": getItemValue('dirtiness'),
+                    "kitchen": getItemValue('kitchen_no')
                },
                "extras": {
                     "inside_oven": extinsideoven,
@@ -104,50 +148,41 @@ function bookingObject(event){
                     "inside_washer": extinsideWasherDryer
                },
                "arrival": {
-                    "date": document.getElementById('arrival_date').value,
-                    "time": document.getElementById('clean_time').value
+                    "date": getItemValue('arrival_date'),
+                    "time": getItemValue('clean_time')
                },
                "recurring": document.querySelector('input[name = "how_often"]:checked').value,
                "user": {
-                    "firstname": document.getElementById('fname').value,
-                    "lastname": document.getElementById('lname').value,
-                    "email": document.getElementById('email').value,
-                    "phone": document.getElementById('phone').value
+                    "firstname": getItemValue('fname'),
+                    "lastname": getItemValue('lname'),
+                    "email": getItemValue('email'),
+                    "phone": getItemValue('phone')
                },
                "location": {
-                    "address": document.getElementById('address').value,
-                    "apt": document.getElementById('apt').value,
-                    "city": document.getElementById('city').value,
-                    "state": document.getElementById('state').value,
-                    "zip": document.getElementById('zip').value
+                    "address": getItemValue('address'),
+                    "apt": getItemValue('apt'),
+                    "city": getItemValue('city'),
+                    "state": getItemValue('state'),
+                    "zip": getItemValue('zip')
                },
-               "discount": document.getElementById('discount_code').value,
-               "comments": document.getElementById('special_instructions').value,
+               "discount": getItemValue('discount_code'),
+               "comments": getItemValue('special_instructions'),
                "card_details": {
-                    "card_number": document.getElementById('card_number').value,
+                    "card_number": getItemValue('card_number'),
                     "month": expiry_month,
                     "year": expiry_year,
-                    "cvc": document.getElementById('cvc').value
+                    "cvc": getItemValue('cvc')
                },
                "terms_and_conditions": document.getElementById('tandc').checked,
                "total_price": tot_price
           };
-          const discount = {
-               "code": document.getElementById('discount_code').value,
-               "total_price": tot_price
-          }
+     const discount = {
+          "code": getItemValue('discount_code'),
+          "total_price": tot_price
+     }
      
-          console.clear();
-          console.log(pay.extras.inside_oven);
-          console.log(pay.extras.walls);
-          console.log(pay.extras.inside_windows);
-          console.log(pay.extras.inside_the_fridge);
-          console.log(pay.extras.organization);
-          console.log(pay.extras.inside_cabinets);
-          console.log(pay.extras.inside_dishwasher);
-          console.log(pay.extras.inside_garage);
-          console.log(pay.extras.microwave);
-          console.log(pay.extras.laundry);
-          console.log(pay.extras.blinds);
-          console.log(pay.extras.inside_washer);
+     console.clear();
+     console.log(pay);
+     console.log(getItemValue('servicetype'));
+     console.log(getTotalPrice());
 }
